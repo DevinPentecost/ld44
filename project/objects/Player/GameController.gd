ab123462554ab6807f5aca6fb6ee3000fe987282
Devin Pentecost <devinpentecost@gmail.com>
2019-04-28 21:11:51 -0700

    Initial staging for endslate

--- project/objects/Player/GameController.gd
+++ project/objects/Player/GameController.gd
@@ -1,11 +1,16 @@
 extends Spatial
 
+signal race_finished
+
 onready var _tween = $Tween
 
 #Keep track of the time
 var time = 0
 var finish_time = 0
 var brake_state = false
+
+#Grab nodes
+onready var _fuel_meter = $FuelMeter
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,22 +29,9 @@ func _process(delta):
 		var time_stamp = "%02d : %02d" % [minutes, seconds]
 		$TimerLabel.text = time_stamp
 		
-	
-	# handle syringe rotating - based on player brake state
-	if brake_state != $Player.movement_state.is_braking():
-		brake_state = !brake_state
-		var fuel_angle = $FuelMeter.rect_rotation
-		_tween.stop_all()
-		if brake_state:
-			_tween.interpolate_property($FuelMeter, "rect_rotation", fuel_angle, 27, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
-		else:
-			_tween.interpolate_property($FuelMeter, "rect_rotation", fuel_angle ,0, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
-		_tween.start()
-			
-		
 	#TEST TO END GAME EARLY
-	#if time > 3 and finish_time == 0:
-	#	_end_race()
+	if time > 3 and finish_time == 0:
+		_end_race()
 	
 func _start_race():
 	
@@ -50,17 +42,10 @@ func _end_race():
 	#Lock the time
 	finish_time = time
 	
+	#Emit signal for listeners
+	emit_signal("race_finished")
+	
 	#Let the player know they won
-	
-	#Hide game-time UI elements
-	_tween.interpolate_property($Progress, "rect_position.x", $Progress.rect_position.x, -100, 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-	
-	
-	#Show leaderboard info, let player submit
-	$WinScreen.visible = true
-		
-	$Tween.interpolate_property($WinScreen, "rect_position.y", $WinScreen.rect_position.y, $WinScreen.rect_position.y + 724, 0.5, Tween.TRANS_QUAD,Tween.TRANS_BOUNCE)
-	$Tween.start()
 	
 	#Lock camera, move player off screen
 	
@@ -71,8 +56,9 @@ func _end_race():
 
 
 func _on_TrackFollower_track_completed():
-	pass # Replace with function body.
-
+	
+	#Begin end-of-game sequence
+	_end_race()
 
 func _on_Player_player_force_brake(start):
 	
@@ -104,3 +90,14 @@ func _on_ViewLB_button_up():
 
 func _on_QuitButton_button_up():
 	get_tree().quit()
+
+func _on_Player_player_brake(start, forced):
+	
+	#Was the player forced to brake?
+	if forced:
+		#Move the bloodletting graphic?
+		$Bloodletting.set_show(start)
+	
+	#Tell the health thing we're braking
+	_fuel_meter.tilt(start)
+	
--- project/objects/UI/Progress.gd
+++ project/objects/UI/Progress.gd
@@ -9,17 +9,30 @@ onready var _track = get_node(track)
 
 onready var _progress = $Progress
 
+var locked = false
+
 func _ready():
 	set_process(true)
 	
 func _process(delta):
 	
+	if locked:
+		return
+	
 	#Set position
 	var position = lerp(start_pos, end_pos, _track.completion)
 	_progress.rect_position = position
 
-
-func _on_TrackFollower_track_completed():
-	
+func _show(showing):
+	if locked:
+		return
+		
+	#Hide always because I'm lazy
 	$Tween.interpolate_property(self, "rect_position", rect_position, Vector2(-300, rect_position.y), 1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	$Tween.start()
+
+
+func _on_MainGame_race_finished():
+	_show(false)
+	locked = true
+	pass # Replace with function body.
